c727ee09effaf135400d3592c87cc2f60635f1bc
DongYeon Kang <myjjue00@gmail.com>
2023-01-16 11:44:15 +0900

    Initial commit

---
+++ src/main/java/com/example/financialfinalproject/FinancialFinalProjectApplication.java
@@ -0,0 +1,13 @@
+package com.example.financialfinalproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinancialFinalProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FinancialFinalProjectApplication.class, args);
+    }
+
+}
