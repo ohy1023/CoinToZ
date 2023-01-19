9c88bc31cc292f9496d9f269fb3a6754f46f75fa
DongYeon Kang <myjjue00@gmail.com>
2023-01-19 17:11:22 +0900

    Feat : Upbit에서 마켓정보 UI 페이징 처리

--- src/main/java/com/example/financialfinalproject/controller/restController/MarketController.java
+++ src/main/java/com/example/financialfinalproject/controller/restController/MarketController.java
@@ -4,6 +4,9 @@ import com.example.financialfinalproject.domain.upbit.dto.MarketDto;
 import com.example.financialfinalproject.service.UpbitService;
 import io.swagger.annotations.ApiOperation;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,9 +22,18 @@ public class MarketController {
 
     @GetMapping("/market")
     // @ApiOperation(value = "마켓 코드 조회", notes = "업비트에서 거래 가능한 마켓 목록")
-    public String getMarket(Model model, @RequestParam(defaultValue = "false") Boolean isDetails){
+    public String getMarket(Model model, Pageable pageable, @RequestParam(defaultValue = "false") Boolean isDetails){
         List<MarketDto> markets = upbitService.getMarket(isDetails);
-        model.addAttribute("markets", markets);
+
+        //List -> Page
+        final int start = (int)pageable.getOffset();
+        final int end = Math.min((start + pageable.getPageSize()), markets.size());
+        final Page<MarketDto> marketsPage = new PageImpl<>(markets.subList(start, end), pageable, markets.size());
+
+        model.addAttribute("markets", marketsPage);
+        model.addAttribute("previous", pageable.previousOrFirst().getPageNumber());
+        model.addAttribute("next", pageable.next().getPageNumber());
+
         return "market";
     }
 
