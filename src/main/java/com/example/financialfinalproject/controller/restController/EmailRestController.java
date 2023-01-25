84a4478b203793f6f7460ec12c648304ff1a83ae
sangho527 <sy980527@naver.com>
2023-01-25 17:25:05 +0900

    Feat: Email 인증 메일 구현
    ---
    네이버 이메일로 인증번호 오는 것 까지 구현
    ---

---
+++ src/main/java/com/example/financialfinalproject/controller/restController/EmailRestController.java
@@ -0,0 +1,22 @@
+package com.example.financialfinalproject.controller.restController;
+
+import com.example.financialfinalproject.service.EmailService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.*;
+
+@Slf4j
+@RequiredArgsConstructor
+@RestController
+public class EmailRestController {
+
+    private final EmailService emailService;
+
+    @PostMapping("login/mailConfirm")
+    @ResponseBody
+    public String mailConfirm(@RequestParam String email) throws Exception {
+        String code = emailService.sendSimpleMessage(email);
+        log.info("인증코드 : " + code);
+        return code;
+    }
+}
