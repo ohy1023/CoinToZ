4b5d53e79376b77817839e94d2b1ddc2c1c0b828
ohy1023 <zvyg1023@naver.com>
2023-01-26 14:58:21 +0900

    Feat : 로그인 화면 추가

--- src/main/frontend/src/App.js
+++ src/main/frontend/src/App.js
@@ -3,6 +3,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import {Navbar,Nav} from 'react-bootstrap';
 import {Route, Routes, useNavigate} from 'react-router-dom';
 import Joinform from './pages/Joinform';
+import SignIn from './pages/SignIn';
 
 
 function App() {
@@ -15,12 +16,13 @@ function App() {
                 <Navbar.Brand style={{margin:'10px'}}><Nav.Link onClick={()=>{navigate('/')}} >Justock</Nav.Link></Navbar.Brand>
                 <Nav className="me-auto">
                     <Nav.Link onClick={()=>{navigate('/join')}}>회원가입</Nav.Link>
-                    {/* <Nav.Link onClick={()=>{navigate('/login')}}>로그인</Nav.Link> */}
+                    <Nav.Link onClick={()=>{navigate('/login')}}>로그인</Nav.Link>
                 </Nav>
             </Navbar>
             <Routes>
                 <Route path='/' element={<>메인 페이지</>}/>
                 <Route path='/join' element={<Joinform/>}/>
+                <Route path='/login' element={<SignIn/>}/>
             </Routes>
         </>
     );
