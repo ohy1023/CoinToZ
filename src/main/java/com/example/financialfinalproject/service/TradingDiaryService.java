214156f96411496b1a69bfb25522823cb9521fbe
강수빈 <tnbin98@gmail.com>
2023-01-31 11:57:41 +0900

    Fix : upbit 입금 구현 UpbitJwtService, UpbitToken 구현

--- src/main/java/com/example/financialfinalproject/service/TradingDiaryService.java
+++ src/main/java/com/example/financialfinalproject/service/TradingDiaryService.java
@@ -1,7 +1,5 @@
 package com.example.financialfinalproject.service;
 
-import com.example.financialfinalproject.domain.entity.TradingDiary;
-import com.example.financialfinalproject.domain.upbit.exchange.OrderResponse;
 import com.example.financialfinalproject.repository.TradingDiaryRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -10,33 +8,35 @@ import org.springframework.stereotype.Service;
 @Service
 public class TradingDiaryService {
 
-private final TradingDiaryRepository tradingDiaryRepository;
+    private final TradingDiaryRepository tradingDiaryRepository;
 
-public void write(OrderResponse orderResponse) {
+}
 
-    // 수수료 반올림
-    int fee = (int) Math.round(orderResponse.getPaid_fee());
-
-    // 매수
-    if (orderResponse.getSide().equals("bid")) {
-
-        TradingDiary tradingDiary = TradingDiary.builder()
-                .created_at(orderResponse.getCreated_at())
-                .uuid(orderResponse.getUuid())
-                .market(orderResponse.getMarket())
-                .sid(orderResponse.getSide())
-                .volume(orderResponse.getVolume())
-                .paid_fee(orderResponse.getPaid_fee())
-                .bid_price(orderResponse.getPrice() + fee)
-                .build();
-
-        tradingDiaryRepository.save(tradingDiary);
-    }
-
-    if (orderResponse.getSide().equals("ask")) {
-
-
-    }
-
+//public void write(OrderResponse orderResponse) {
+//
+//    // 수수료 반올림
+//    int fee = (int) Math.round(orderResponse.getPaid_fee());
+//
+//    // 매수
+//    if (orderResponse.getSide().equals("bid")) {
+//
+//        TradingDiary tradingDiary = TradingDiary.builder()
+//                .created_at(orderResponse.getCreated_at())
+//                .uuid(orderResponse.getUuid())
+//                .market(orderResponse.getMarket())
+//                .sid(orderResponse.getSide())
+//                .volume(orderResponse.getVolume())
+//                .paid_fee(orderResponse.getPaid_fee())
+//                .bid_price(orderResponse.getPrice() + fee)
+//                .build();
+//
+//        tradingDiaryRepository.save(tradingDiary);
+//    }
+//
+//    if (orderResponse.getSide().equals("ask")) {
+//
+//
+//    }
+//
 
 
