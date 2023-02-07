34c9d56a070a9e9fe1a8cb27aac2a2416f732035
ohy1023 <zvyg1023@naver.com>
2023-02-07 21:38:37 +0900

    Refactor : 매매일지 중간 커밋

--- src/main/java/com/example/financialfinalproject/service/TradingDiaryService.java
+++ src/main/java/com/example/financialfinalproject/service/TradingDiaryService.java
@@ -2,10 +2,13 @@ package com.example.financialfinalproject.service;
 
 import com.example.financialfinalproject.domain.dto.TradingDiaryDto;
 import com.example.financialfinalproject.domain.entity.TradingDiary;
+import com.example.financialfinalproject.domain.entity.User;
+import com.example.financialfinalproject.domain.response.MyCoinCntResponse;
 import com.example.financialfinalproject.domain.upbit.exchange.OrderResponse;
 import com.example.financialfinalproject.exception.AppException;
 import com.example.financialfinalproject.exception.ErrorCode;
 import com.example.financialfinalproject.repository.TradingDiaryRepository;
+import com.example.financialfinalproject.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -14,16 +17,53 @@ import org.springframework.stereotype.Service;
 import java.util.List;
 import java.util.Optional;
 
+import static com.example.financialfinalproject.exception.ErrorCode.EMAIL_NOT_FOUND;
+
 @RequiredArgsConstructor
 @Service
 public class TradingDiaryService {
 
     private final TradingDiaryRepository tradingDiaryRepository;
+    private final UserRepository userRepository;
+
+    private String BitCoin = "KRW-BTC";
+    private String Ethereum = "KRW-ETH";
+    private String Ripple = "KRW-XRP";
+    private String ADA = "KRW-ADA";
+    private String DogeCoin = "KRW-DOGE";
 
 
-    public List<TradingDiary> listOf() {
-        List<TradingDiary> all = tradingDiaryRepository.findAll();
-        return all;
+    public List<TradingDiary> listOf(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    throw new AppException(EMAIL_NOT_FOUND, EMAIL_NOT_FOUND.getMessage());
+                });
+        List<TradingDiary> diaryListOfUser = tradingDiaryRepository.findAllByUser(user);
+        return diaryListOfUser;
+    }
+
+    public MyCoinCntResponse getCoins(String email) {
+        User user = userRepository.findByEmail(email)
+                .orElseThrow(() -> {
+                    throw new AppException(EMAIL_NOT_FOUND, EMAIL_NOT_FOUND.getMessage());
+                });
+
+        double bitCoinCnt = tradingDiaryRepository.findSumVolumeByMarketContainingAndUser(BitCoin, user);
+        double ethereumCnt = tradingDiaryRepository.findSumVolumeByMarketContainingAndUser(Ethereum, user);
+        double rippleCnt = tradingDiaryRepository.findSumVolumeByMarketContainingAndUser(Ripple, user);
+        double ADACnt = tradingDiaryRepository.findSumVolumeByMarketContainingAndUser(ADA, user);
+        double dogeCoinCnt = tradingDiaryRepository.findSumVolumeByMarketContainingAndUser(DogeCoin, user);
+        double etcCnt = tradingDiaryRepository.findSumVolumeByUser(user) - (bitCoinCnt + ethereumCnt + rippleCnt + ADACnt + dogeCoinCnt);
+
+        return MyCoinCntResponse.builder()
+                .bitCoin(bitCoinCnt)
+                .ethereum(ethereumCnt)
+                .ADA(ADACnt)
+                .dogeCoin(dogeCoinCnt)
+                .ripple(rippleCnt)
+                .etc(etcCnt)
+                .build();
+
     }
 
     public void write(OrderResponse orderResponse) {
@@ -40,7 +80,7 @@ public class TradingDiaryService {
                     .bid_created_at(orderResponse.getCreated_at()) // 매수 주문시간
                     .market(orderResponse.getMarket())
                     .bid_price(orderResponse.getPrice() + fee) // 매수가격(수수료 포함)
-                    .volume(orderResponse.getExecuted_volume()) // 수량
+                    .volume(Double.parseDouble(orderResponse.getExecuted_volume())) // 수량
                     .build();
 
             tradingDiaryRepository.save(tradingDiary);
@@ -54,10 +94,10 @@ public class TradingDiaryService {
             tradingDiarys.setAsk_price(orderResponse.getPrice() + fee); // 매도가격(수수료 포함)
 
 
-            tradingDiarys.setArbitrage(tradingDiarys.getAsk_price()- tradingDiarys.getBid_price()); // 차익 (매수가격 - 매도가격)
+            tradingDiarys.setArbitrage(tradingDiarys.getAsk_price() - tradingDiarys.getBid_price()); // 차익 (매수가격 - 매도가격)
 
-            double revenue = ((double)tradingDiarys.getBid_price() / (double)tradingDiarys.getArbitrage()); // 수익률 계산
-            tradingDiarys.setRevenue(Math.round(revenue*100)/100.0); // 수익률 (소숫점 2자리 까지 표현)
+            double revenue = ((double) tradingDiarys.getBid_price() / (double) tradingDiarys.getArbitrage()); // 수익률 계산
+            tradingDiarys.setRevenue(Math.round(revenue * 100) / 100.0); // 수익률 (소숫점 2자리 까지 표현)
 
             tradingDiaryRepository.save(tradingDiarys);
         }
@@ -66,7 +106,7 @@ public class TradingDiaryService {
     public TradingDiaryDto edit(Long id, String comment) {
 
         Optional<TradingDiary> tradingDiary = tradingDiaryRepository.findById(id);
-        tradingDiary.orElseThrow(()-> new AppException(ErrorCode.DIARY_NOT_FOUND,"해당 일지가 없습니다."));
+        tradingDiary.orElseThrow(() -> new AppException(ErrorCode.DIARY_NOT_FOUND, "해당 일지가 없습니다."));
 
         TradingDiary tradingDiaryEdit = tradingDiary.get();
 
@@ -81,16 +121,16 @@ public class TradingDiaryService {
     }
 
 
-    public Page<TradingDiary> list(PageRequest pageRequest){
-    Page<TradingDiary> tradingDiaryList = tradingDiaryRepository.findAll(pageRequest);
-    return tradingDiaryList;
+    public Page<TradingDiary> list(PageRequest pageRequest) {
+        Page<TradingDiary> tradingDiaryList = tradingDiaryRepository.findAll(pageRequest);
+        return tradingDiaryList;
     }
 
 
-    public TradingDiaryDto delete(Long id){
+    public TradingDiaryDto delete(Long id) {
 
-       Optional<TradingDiary> tradingDiary = tradingDiaryRepository.findById(id);
-       tradingDiary.orElseThrow(()-> new AppException(ErrorCode.DIARY_NOT_FOUND,"해당 일지가 없습니다."));
+        Optional<TradingDiary> tradingDiary = tradingDiaryRepository.findById(id);
+        tradingDiary.orElseThrow(() -> new AppException(ErrorCode.DIARY_NOT_FOUND, "해당 일지가 없습니다."));
 
         tradingDiaryRepository.deleteById(id);
 
