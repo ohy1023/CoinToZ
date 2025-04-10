d21fcb8ddd7f69da40e93dcc83a01f91786d34a6
오형상 <vyg1023@gmail.com>
2025-04-10 15:45:39 +0900

    [Chore] 설정 변경

--- frontend/src/setProxy.js
+++ frontend/src/setProxy.js
@@ -6,8 +6,7 @@ module.exports = function (app) {
   app.use(
     '/api', // 프론트엔드에서 '/api'로 요청을 보내면, 백엔드인 8080포트(=target)로 요청이 도착
     createProxyMiddleware({
-      // target: 'http://ec2-52-78-23-203.ap-northeast-2.compute.amazonaws.com:8080',	// 서버 URL or localhost:설정한 포트번호
-      target: 'http://localhost:8081', // 서버 URL or localhost:설정한 포트번호
+      target: 'http://211.188.59.148:8080', // 서버 URL or localhost:설정한 포트번호
       changeOrigin: true,
     })
   );
