333157e07fca3944548fb81995111d1c6b9a2292
오형상 <vyg1023@gmail.com>
2025-04-12 17:18:19 +0900

    Feat : Redis 비밀번호 추가

--- backend/src/main/java/com/example/financialfinalproject/global/config/RedisConfig.java
+++ backend/src/main/java/com/example/financialfinalproject/global/config/RedisConfig.java
@@ -3,6 +3,7 @@ package com.example.financialfinalproject.global.config;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisStandaloneConfiguration;
 import org.springframework.data.redis.connection.lettuce.LettuceConnectionFactory;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.data.redis.serializer.StringRedisSerializer;
@@ -16,10 +17,18 @@ public class RedisConfig {
     @Value("${spring.redis.port}")
     private int port;
 
+    @Value("${spring.redis.password}")
+    private String password;
+
     @Bean
     public LettuceConnectionFactory redisConnectionFactory() {
-        return new LettuceConnectionFactory(host, port);
+        RedisStandaloneConfiguration config = new RedisStandaloneConfiguration();
+        config.setHostName(host);
+        config.setPort(port);
+        config.setPassword(password);
+        return new LettuceConnectionFactory(config);
     }
+
 
     @Bean
     public RedisTemplate<?, ?> redisTemplate() {
